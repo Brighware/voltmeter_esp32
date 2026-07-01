fd9c8d9e5c2aaf491cdacb67f4076772191638d9
Brighware <brigh@brighware.com>
2026-07-01 16:05:13 -0400

    First commit

---
+++ main/oled.h
@@ -0,0 +1,10 @@
+#ifndef __OLED_H_USED__
+#define __OLED_H_USED__
+
+extern void init_oled(void);
+extern void exec_oled(void);
+extern void oled_demo_ui(lv_display_t *disp);
+extern void print_text(lv_display_t *disp, char* txt);
+
+
+#endif
